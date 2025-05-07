6319857e24579f5cfc61ee2a31b824519c1cc3a8
Crakchi <crakchi@gmail.com>
2025-05-07 08:47:16 -0500

    feat: Se agrego la grafica con mathplot

--- src_python/app.py
+++ src_python/app.py
@@ -1,29 +1,52 @@
 # Ejecutar BenchMarking
-#import benchmarking as Ben
+
+import matplotlib.pyplot as plt
+import random
+import time
 
 from benchmarking import Benchmarking
 from metodos_ordenamiento import MetodosOrdenamiento
 
 if __name__ == "__main__":
-    size = 10000
-    
-    benchM = Benchmarking
-    metodosOr = MetodosOrdenamiento
-
-    arreglo = benchM.build_arreglo(size)
-
-    
-    metodos = { # Diccionario 
-        "bubuja": metodosOr.sortByBubble,
-        "seleccion": metodosOr.sort_seleccion,
-    }
     resultados = []
-    
-    for nombre, metodo in metodos.items():# como un foreach que devuelve la clave,  valor
-        tiempo = Benchmarking.medir_tiempo(metodo, arreglo)
-        tuplaResultado = (size,nombre,tiempo)
-        resultados.append(tuplaResultado)
-    for resultado in resultados:
-        size, nombre, tiempo = resultado
-        print(f"Tamaño: {size}, Nombre: {nombre}, Tiempo: {tiempo:.6f} segundos")
+    tamanios = [500, 1000, 2000]
 
+    for tam in tamanios:
+        benchM = Benchmarking()
+        metodosOr = MetodosOrdenamiento()
+
+        arreglo = benchM.build_arreglo(tam)
+
+        metodos = {  # Diccionario
+            "burbuja": metodosOr.sortByBubble,
+            "seleccion": metodosOr.sort_seleccion,
+        }
+
+        for nombre, metodo in metodos.items():  
+            tiempo = benchM.medir_tiempo(metodo, arreglo)  
+            tuplaResultado = (tam, nombre, tiempo)
+            resultados.append(tuplaResultado)
+
+        for resultado in resultados:
+            tam, nombre, tiempo = resultado
+            print(f"Tamaño: {tam}, Nombre: {nombre}, Tiempo: {tiempo:.6f} segundos")
+
+    tiempos_by_metodo = {
+        "burbuja": [],
+        "seleccion": [],
+    }
+
+    # Clasificar los métodos según el tiempo
+    for tam, nombre, tiempo in resultados:
+        tiempos_by_metodo[nombre].append(tiempo)
+
+    # Crear una gráfica
+    plt.figure(figsize=(10, 6))
+
+    # Gráfica de tiempos para cada método
+    for nombre, tiempo in tiempos_by_metodo.items():
+        plt.plot(tamanios, tiempo, label=nombre, marker='o')
+
+    plt.grid()
+    plt.legend()
+    plt.show()
---
+++ src_python/grafica.py
@@ -0,0 +1,15 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5]
+y = [2,4,6,8,10]
+
+nombre_linea = "Linea 1"
+plt.plot(x,y,label=nombre_linea,color="blue")
+
+# agregar parametros
+plt.title("Primeros Graficos")
+plt.xlabel("Datos eje X")
+plt.ylabel("Datos en eje Y")
+plt.legend()
+plt.show()
